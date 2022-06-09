f587f92b7728479b23f74cffa1e7577e4ff2f02e
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-06-09 13:04:12 +0200

    Replace Class with Object in Comments Repository

--- src/db/comments.repository.js
+++ src/db/comments.repository.js
@@ -1,29 +1,29 @@
 const db = require('./models');
 const Comment = db.comment;
 
-class CommentsRepository {
-  static async create(commentBody, userId, articleId) {
+const CommentsRepository = {
+  async create(commentBody, userId, articleId) {
     return await new Comment({
       body: commentBody,
       author: userId,
       article: articleId,
     }).save();
-  }
+  },
 
-  static async findByArticleId(articleId) {
+  async findByArticleId(articleId) {
     return await Comment.find({article: articleId})
         .populate('author', 'image username bio following')
         .sort([['updatedAt', 'descending']])
         .exec();
-  }
+  },
 
-  static async deleteOneById(id) {
+  async deleteOneById(id) {
     return await Comment.deleteOne({_id: id}).exec();
-  }
+  },
 
-  static async findOneBy(field, value) {
+  async findOneBy(field, value) {
     return await Comment.findOne({[field]: value}).exec();
-  }
-}
+  },
+};
 
 module.exports = CommentsRepository;
