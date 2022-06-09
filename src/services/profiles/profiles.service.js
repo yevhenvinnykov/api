bff9e1aad2bb8f4c331aa99833bfd83495172588
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-06-09 14:12:10 +0200

    Refactor Profiles Service

--- src/services/profiles/profiles.service.js
+++ src/services/profiles/profiles.service.js
@@ -4,27 +4,29 @@ const UsersRepository = require('../../db/users.repository');
 
 const ProfilesService = {
   async getProfile(authUserId, username) {
-    const [authUser, profile] = await this
-        .fetchDataFromDB(authUserId, username);
-    const isProfileFollowed = authUser?.following
-        .some((u) => u._id.equals(profile._id));
+    const [authUser, profile] = await this.fetchDataFromDB(authUserId, username);
+
+    const isProfileFollowed = !!authUser && authUser.following.some((id) => id.equals(profile._id));
+
     return {profile: {...profile._doc, following: isProfileFollowed}};
   },
 
   async followProfile(authUserId, username) {
-    const [authUser, profile] = await this
-        .fetchDataFromDB(authUserId, username);
-    if (!authUser?.following.some((u) => u.equals(profile._id))) {
+    const [authUser, profile] = await this.fetchDataFromDB(authUserId, username);
+
+    if (!authUser?.following.some((id) => id.equals(profile._id))) {
       await UsersRepository.follow(authUser, profile._id);
     }
+
     return {profile: {...profile._doc, following: true}};
   },
 
   async unfollowProfile(authUserId, username) {
-    const [authUser, profile] = await this
-        .fetchDataFromDB(authUserId, username);
-    const index = authUser?.following.findIndex((id) => id.equals(profile._id));
+    const [authUser, profile] = await this.fetchDataFromDB(authUserId, username);
+
+    const index = authUser ? authUser.following.findIndex((id) => id.equals(profile._id)) : -1;
     if (index !== -1) await UsersRepository.unfollow(authUser, index);
+
     return {profile: {...profile._doc, following: false}};
   },
 
@@ -32,7 +34,9 @@ const ProfilesService = {
     const authUser = await UsersRepository.findOneBy('_id', authUserId, ' ');
     const profile = await UsersRepository
         .findOneBy('username', username, 'username email bio image');
+
     if (!authUser || !profile) throw new NotFoundError('User not found');
+
     return [authUser, profile];
   },
 };
