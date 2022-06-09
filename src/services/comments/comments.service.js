bf0c962553f58f2df7174ed7c3ced5e5ce3adc5e
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-06-09 17:20:53 +0200

    Add New Lines Between Blocks

--- src/services/comments/comments.service.js
+++ src/services/comments/comments.service.js
@@ -6,28 +6,34 @@ const CommentsService = {
   async createComment({authUserId, slug, commentBody}) {
     const article = await ArticlesRepository.findOneBy('slug', slug);
     if (!article) throw new NotFoundError('Article not found');
+
     const comment = await CommentsRepository
         .create(commentBody, authUserId, article._id);
     if (!comment) {
       throw new BadRequestError('Something went wrong when creating comment');
     }
+
     return comment;
   },
 
   async getComments(slug) {
     const article = await await ArticlesRepository.findOneBy('slug', slug);
     if (!article) throw new NotFoundError('Article not found');
+
     const comments = await CommentsRepository.findByArticleId(article._id);
     if (!comments) throw new NotFoundError('Comments not found');
+
     return comments;
   },
 
   async deleteComment(commentId, authUserId) {
     const comment = await CommentsRepository.findOneBy('_id', commentId);
     if (!comment) throw new NotFoundError('Comment not found');
+
     if (!comment.author.equals(authUserId)) {
       throw new BadRequestError('You are not authorized');
     }
+
     const {deletedCount} = await CommentsRepository.deleteOneById(commentId);
     if (!deletedCount) throw new BadRequestError('Something went wrong');
   },
--- src/services/users/users.service.js
+++ src/services/users/users.service.js
@@ -6,34 +6,42 @@ const UsersRepository = require('../../db/users.repository');
 const UsersService = {
   async createUser({userData}) {
     await UsersRepository.create(userData);
+
     const user = await UsersRepository.findOneBy('email', userData.email);
     if (!user) {
       throw new BadRequestError('Something went wrong when creating the user');
     }
     user.token = this.createToken(user.id);
+
     return user;
   },
 
   async getLoggedInUser({authUserId}) {
     const user = await UsersRepository.findOneBy('_id', authUserId);
     if (!user) throw new NotFoundError('User not found');
+
     return user;
   },
 
   async updateUser({authUserId, userData}) {
     const user = await UsersRepository.findOneBy('_id', authUserId);
     if (!user) throw new NotFoundError('User not found');
+
     await UsersRepository.update(user, userData);
     user.token = this.createToken(user.id);
+
     return user;
   },
 
   async logIn(email, password) {
     let user = await UsersRepository.findOneBy('email', email, 'password');
     if (!user) throw new NotFoundError('User not found');
+
     this.validatePassword(password, user.password);
+
     user = await UsersRepository.findOneBy('email', email);
     user.token = this.createToken(user.id);
+
     return user;
   },
 
