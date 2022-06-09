840f6590134823f35d4575d51546d3222aeb9f8b
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-06-09 13:59:24 +0200

    Replace Class with Object in Profiles Service

--- src/services/profiles/profiles.service.js
+++ src/services/profiles/profiles.service.js
@@ -2,39 +2,39 @@ const {NotFoundError} = require('../../utils/errorHandler');
 const UsersRepository = require('../../db/users.repository');
 
 
-class ProfilesService {
-  static async getProfile(authUserId, username) {
-    const [authUser, profile] = await ProfilesService
-        .#fetchDataFromDB(authUserId, username);
+const ProfilesService = {
+  async getProfile(authUserId, username) {
+    const [authUser, profile] = await this
+        .fetchDataFromDB(authUserId, username);
     const isProfileFollowed = authUser?.following
         .some((u) => u._id.equals(profile._id));
     return {profile: {...profile._doc, following: isProfileFollowed}};
-  }
+  },
 
-  static async followProfile(authUserId, username) {
-    const [authUser, profile] = await ProfilesService
-        .#fetchDataFromDB(authUserId, username);
+  async followProfile(authUserId, username) {
+    const [authUser, profile] = await this
+        .fetchDataFromDB(authUserId, username);
     if (!authUser?.following.some((u) => u.equals(profile._id))) {
       await UsersRepository.follow(authUser, profile._id);
     }
     return {profile: {...profile._doc, following: true}};
-  }
+  },
 
-  static async unfollowProfile(authUserId, username) {
-    const [authUser, profile] = await ProfilesService
-        .#fetchDataFromDB(authUserId, username);
+  async unfollowProfile(authUserId, username) {
+    const [authUser, profile] = await this
+        .fetchDataFromDB(authUserId, username);
     const index = authUser?.following.findIndex((id) => id.equals(profile._id));
     if (index !== -1) await UsersRepository.unfollow(authUser, index);
     return {profile: {...profile._doc, following: false}};
-  }
+  },
 
-  static async #fetchDataFromDB(authUserId, username) {
+  async fetchDataFromDB(authUserId, username) {
     const authUser = await UsersRepository.findOneBy('_id', authUserId, ' ');
     const profile = await UsersRepository
         .findOneBy('username', username, 'username email bio image');
     if (!authUser || !profile) throw new NotFoundError('User not found');
     return [authUser, profile];
-  }
+  },
 };
 
 module.exports = ProfilesService;
