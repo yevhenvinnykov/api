daf37a68c7204c9622854fd8e9310f2c81e48da8
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-06-09 13:12:21 +0200

    Replace Class with Object in Articles Controller

--- src/controllers/articles.controller.js
+++ src/controllers/articles.controller.js
@@ -2,10 +2,10 @@ const ArticlesService = require('../services/articles/articles.service');
 const {ErrorHandler} = require('../utils/errorHandler');
 
 
-class ArticlesController {
-  static async handleArticleCRUD(req, res) {
+const ArticlesController = {
+  async handleArticleCRUD(req, res) {
     try {
-      const data = ArticlesController.#extractDataFromReq(req);
+      const data = this.extractDataFromReq(req);
       let action;
       switch (req.method) {
         case 'POST': action = 'createArticle';
@@ -21,20 +21,20 @@ class ArticlesController {
     } catch (error) {
       ErrorHandler.catchError(res, error);
     }
-  }
+  },
 
-  static async handleArticleLikeDislike(req, res) {
+  async handleArticleLikeDislike(req, res) {
     try {
-      const {slug, authUserId} = ArticlesController.#extractDataFromReq(req);
+      const {slug, authUserId} = this.extractDataFromReq(req);
       const action = req.method === 'POST' ? 'likeArticle' : 'dislikeArticle';
       const article = await ArticlesService[action](slug, authUserId);
       res.status(200).json({article});
     } catch (error) {
       ErrorHandler.catchError(res, error);
     }
-  }
+  },
 
-  static async getArticles(req, res) {
+  async getArticles(req, res) {
     try {
       const action = req.route.path === '/api/articles' ?
        'getArticles' :
@@ -45,25 +45,25 @@ class ArticlesController {
     } catch (error) {
       ErrorHandler.catchError(res, error);
     }
-  }
+  },
 
-  static async getTags(req, res) {
+  async getTags(req, res) {
     try {
       const tags = await ArticlesService.getTags();
       res.status(200).json({tags});
     } catch (error) {
       ErrorHandler.catchError(res, error);
     }
-  }
+  },
 
-  static #extractDataFromReq(req) {
+  extractDataFromReq(req) {
     const data = {};
     data.slug = req.params?.slug;
     data.authUserId = req?.userId;
     data.updateData = req.body?.article;
     data.articleData = req.body?.article;
     return data;
-  }
-}
+  },
+};
 
 module.exports = ArticlesController;
