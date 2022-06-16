064ec53bf109e494b9a7e127243c7506d77223da
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-06-16 14:24:19 +0200

    Refactor Articles CRUD Router Tests

--- src/__tests__/articles/articles-crud.router.test.js
+++ src/__tests__/articles/articles-crud.router.test.js
@@ -1,34 +1,25 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
-const User = require('../../db/models/user.model');
-const Article = require('../../db/models/article.model');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 const app = require('../index');
+
+const MockCreator = require('../utils/MockCreator');
+const cleanUpDB = require('../utils/cleanUpDB');
 
 describe('ARTICLES CRUD ROUTER', () => {
   let server;
   let body;
   let user;
-  let token;
+  let articleTitle = 'Lorem';
 
   beforeAll(async () => {
-    await Article.deleteMany({});
-    await User.deleteMany({});
+    await cleanUpDB();
     server = app.listen(3005);
   });
 
   afterAll(async () => {
-    await Article.deleteMany({});
-    await User.deleteMany({});
+    await cleanUpDB();
     await mongoose.connection.close();
     await server.close();
-  });
-
-  it('should have a module', () => {
-    expect(User).toBeDefined();
-    expect(Article).toBeDefined();
-    expect(app).toBeDefined();
   });
 
   beforeAll(() => {
@@ -43,16 +34,7 @@ describe('ARTICLES CRUD ROUTER', () => {
   });
 
   beforeAll(async () => {
-    process.env.JWT_SECRET = 'secret';
-    // Have no idea why, but it won't work without setting the env variable
-    user = await new User({
-      username: 'Ross',
-      email: 'ross@email.com',
-      password: bcrypt.hashSync('Pivot1', 8),
-    });
-    token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: 3600});
-    user.token = token;
-    await user.save();
+    user = await MockCreator.createUserMock('Ross');
   });
 
   describe('POST /api/articles', () => {
@@ -60,44 +42,34 @@ describe('ARTICLES CRUD ROUTER', () => {
       const response = await request(server)
           .post('/api/articles')
           .send(body)
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .set('x-access-token', user.token);
 
       expect(response.statusCode).toBe(200);
-      expect(response.body.article.title).toBe('Lorem');
+      expect(response.body.article.title).toBe(articleTitle);
       expect(response.body.article.author).toBe(user.id);
       expect(response.body.article._id).toBeTruthy();
     });
 
     it('should fail because no token is provided', async () => {
-      const response = await request(server)
-          .post('/api/articles')
-          .send(body)
-          .set('Accept', 'application/json');
+      const response = await request(server).post('/api/articles').send(body);
 
       expect(response.statusCode).toBe(400);
     });
 
     it('should fail because the article\'s title is not unique', async () => {
-      new Article({
-        title: 'Lorem',
-        description: 'Lorem ipsum',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        tagList: ['lorem'],
-      }).save();
+      await MockCreator.createArticleMock(articleTitle);
 
       const response = await request(server)
           .post('/api/articles')
           .send(body)
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .set('x-access-token', user.token);
 
       expect(response.statusCode).toBe(400);
     });
   });
 
   describe('PUT /api/articles/:slug', () => {
-    it('should update the article', async () => {
+    it('should update the article, ignoring unnecessary fileds', async () => {
       const body = {
         article: {
           title: 'Ipsum',
@@ -106,22 +78,21 @@ describe('ARTICLES CRUD ROUTER', () => {
         },
       };
       const response = await request(server)
-          .put('/api/articles/Lorem')
+          .put(`/api/articles/${articleTitle}`)
           .send(body)
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .set('x-access-token', user.token);
+
+      const {title, description, propToBeIgnored} = response.body.article;
+      articleTitle = title;
 
       expect(response.statusCode).toBe(200);
-      expect(response.body.article.title).toBe('Ipsum');
-      expect(response.body.article.description).toBe('Dolor sit amet!');
-      expect(response.body.article.propToBeIgnored).toBeUndefined();
+      expect(title).toBe('Ipsum');
+      expect(description).toBe('Dolor sit amet!');
+      expect(propToBeIgnored).toBeUndefined();
     });
 
     it('should fail because no token is provided', async () => {
-      const response = await request(server)
-          .put('/api/articles/Lorem')
-          .send(body)
-          .set('Accept', 'application/json');
+      const response = await request(server).put(`/api/articles/${articleTitle}`).send(body);
 
       expect(response.statusCode).toBe(400);
     });
@@ -129,10 +100,9 @@ describe('ARTICLES CRUD ROUTER', () => {
     it('should fail because no article is found', async () => {
       body.article.title = 'new title';
       const response = await request(server)
-          .put('/api/articles/Unknown')
+          .put('/api/articles/UNKNOWN')
           .send(body)
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .set('x-access-token', user.token);
 
       expect(response.statusCode).toBe(404);
     });
@@ -142,11 +112,11 @@ describe('ARTICLES CRUD ROUTER', () => {
     let articleId;
     it('should get the article, favorited: false because the article isn\'t liked', async () => {
       const response = await request(server)
-          .get('/api/articles/Ipsum')
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .get(`/api/articles/${articleTitle}`)
+          .set('x-access-token', user.token);
 
       articleId = response.body.article._id;
+
       expect(response.statusCode).toBe(200);
       expect(response.body.article.title).toBe('Ipsum');
       expect(response.body.article.description).toBe('Dolor sit amet!');
@@ -155,10 +125,10 @@ describe('ARTICLES CRUD ROUTER', () => {
     it('should get the article, favorited: true because the article is liked', async () => {
       user.favorites.push(articleId);
       await user.save();
+
       const response = await request(server)
-          .get('/api/articles/Ipsum')
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .get(`/api/articles/${articleTitle}`)
+          .set('x-access-token', user.token);
 
       expect(response.statusCode).toBe(200);
       expect(response.body.article._id).toBe(articleId);
@@ -166,9 +136,7 @@ describe('ARTICLES CRUD ROUTER', () => {
     });
 
     it('should get the article, favorited: false because no token is provided', async () => {
-      const response = await request(server)
-          .get('/api/articles/Ipsum')
-          .set('Accept', 'application/json');
+      const response = await request(server).get(`/api/articles/${articleTitle}`);
 
       expect(response.statusCode).toBe(200);
       expect(response.body.article._id).toBe(articleId);
@@ -176,9 +144,7 @@ describe('ARTICLES CRUD ROUTER', () => {
     });
 
     it('should fail because no article is found', async () => {
-      const response = await request(server)
-          .get('/api/articles/Unknown')
-          .set('Accept', 'application/json');
+      const response = await request(server).get('/api/articles/UNKNOWN');
 
       expect(response.statusCode).toBe(404);
     });
@@ -186,27 +152,23 @@ describe('ARTICLES CRUD ROUTER', () => {
 
   describe('DELETE /api/articles/:slug', () => {
     it('should fail because no token is provided', async () => {
-      const response = await request(server)
-          .delete('/api/articles/Ipsum')
-          .set('Accept', 'application/json');
+      const response = await request(server).delete(`/api/articles/${articleTitle}`);
 
       expect(response.statusCode).toBe(400);
     });
 
     it('should fail because the article does not exist', async () => {
       const response = await request(server)
-          .delete('/api/articles/Unknown')
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .delete('/api/articles/UNKNOWN')
+          .set('x-access-token', user.token);
 
       expect(response.statusCode).toBe(400);
     });
 
     it('should delete the article', async () => {
       const response = await request(server)
-          .delete('/api/articles/Ipsum')
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .delete(`/api/articles/${articleTitle}`)
+          .set('x-access-token', user.token);
 
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual({});
