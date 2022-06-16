63f504070c5874985b0b9d91e9fe07260f586a8f
Yevhen Vinnykov <yevhen.vinnykov@dev.pro>
2022-06-16 14:09:13 +0200

    Refactor Users Router Tests

--- src/__tests__/users.router.test.js
+++ src/__tests__/users.router.test.js
@@ -1,45 +1,40 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
-const User = require('../db/models/user.model');
-const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const app = require('./index');
+
+const MockCreator = require('./utils/MockCreator');
+const cleanUpDB = require('./utils/cleanUpDB');
 
 describe('USERS ROUTER', () => {
   let server;
 
   beforeAll(async () => {
-    await User.deleteMany({});
+    await cleanUpDB();
     server = app.listen(3001);
   });
 
   afterAll(async () => {
-    await User.deleteMany({});
+    await cleanUpDB();
     await mongoose.connection.close();
     await server.close();
   });
 
-  it('should have a module', () => {
-    expect(User).toBeDefined();
-    expect(app).toBeDefined();
-  });
-
   describe('POST /api/users/signup', () => {
     let body;
+
     beforeEach(() => {
       body = {
         user: {
           username: 'John',
-          email: 'doe@email.com',
-          password: 'password1A',
+          email: 'john@email.com',
+          password: 'JohnPassword1',
         },
       };
     });
+
     it('should create a user and return the user object with a token', async () => {
-      const response = await request(server)
-          .post('/api/users/signup')
-          .send(body)
-          .set('Accept', 'application/json');
+      const response = await request(server).post('/api/users/signup').send(body);
 
       expect(response.statusCode).toBe(200);
       expect(response.body.user.username).toBe('John');
@@ -48,10 +43,7 @@ describe('USERS ROUTER', () => {
 
     it('should fail because the password won\'t pass the validation', async () => {
       body.user.password = 'invld';
-      const response = await request(server)
-          .post('/api/users/signup')
-          .send(body)
-          .set('Accept', 'application/json');
+      const response = await request(server).post('/api/users/signup').send(body);
 
       expect(response.statusCode).toBe(400);
     });
@@ -59,93 +51,57 @@ describe('USERS ROUTER', () => {
 
   it('should fail because no email was provided', async () => {
     body = {user: {username: 'username', password: 'Password1'}};
-    const response = await request(server)
-        .post('/api/users/signup')
-        .send(body)
-        .set('Accept', 'application/json');
+    const response = await request(server).post('/api/users/signup').send(body);
 
     expect(response.statusCode).toBe(500);
   });
 
   it('should fail because the email is invalid', async () => {
     body.user.email = '!!1=+invalidemail';
-    const response = await request(server)
-        .post('/api/users/signup')
-        .send(body)
-        .set('Accept', 'application/json');
+    const response = await request(server).post('/api/users/signup').send(body);
 
     expect(response.statusCode).toBe(400);
   });
 
   describe('PUT /api/users', () => {
     let user;
-    let token;
-
-    beforeEach(async () => {
-      user = await new User({
-        username: 'John',
-        email: 'doe@email.com',
-        password: bcrypt.hashSync('password1A', 8),
-      });
-      token = jwt.sign({id: user._id}, 'secret', {expiresIn: 3600});
-      user.token = token;
-      await user.save();
-    });
 
     beforeAll(async () => {
-      await new User({
-        username: 'Jack',
-        email: 'email@email.com',
-        password: bcrypt.hashSync('MyPassword1', 8),
-      }).save();
+      user = await MockCreator.createUserMock('John');
+      await MockCreator.createUserMock('Jack');
     });
 
     it('should fail because the email is taken', async () => {
-      const body = {
-        user: {
-          username: 'Jack',
-          email: 'email@email.com',
-        },
-      };
+      const body = {user: {username: 'New Username', email: 'jack@email.com'}};
+
       const response = await request(server)
           .put('/api/users')
           .send(body)
-          .set('x-access-token', token)
-          .set('Accept', 'application/json');
+          .set('x-access-token', user.token);
 
       expect(response.statusCode).toBe(400);
     });
 
     it('should fail because the token is invalid', async () => {
-      const body = {
-        user: {
-          username: 'Jack',
-        },
-      };
+      const body = {user: {username: 'New Username'}};
+
       const response = await request(server)
           .put('/api/users')
           .send(body)
-          .set('Accept', 'application/json')
           .set('x-access-token', 'INVALID_TOKEN');
 
       expect(response.statusCode).toBe(400);
     });
 
     it('should fail because the token is not provided', async () => {
-      const body = {
-        user: {
-          username: 'Jack',
-        },
-      };
-      const response = await request(server)
-          .put('/api/users')
-          .send(body)
-          .set('Accept', 'application/json');
+      const body = {user: {username: 'New Username'}};
+
+      const response = await request(server).put('/api/users').send(body);
 
       expect(response.statusCode).toBe(400);
     });
 
-    it('should update the user', async () => {
+    it('should update the user, ignoring unnecessary fileds', async () => {
       const body = {
         user: {
           username: 'Ross',
@@ -156,13 +112,14 @@ describe('USERS ROUTER', () => {
       const response = await request(server)
           .put('/api/users')
           .send(body)
-          .set('Accept', 'application/json')
-          .set('x-access-token', token);
+          .set('x-access-token', user.token);
+
+      const {username, propToBeIgnored, password} = response.body.user;
 
       expect(response.statusCode).toBe(200);
-      expect(response.body.user.username).toBe('Ross');
-      expect(response.body.user.propToBeIgnored).toBeUndefined();
-      expect(bcrypt.compareSync('NewPassword1', response.body.user.password)).toBe(true);
+      expect(username).toBe('Ross');
+      expect(propToBeIgnored).toBeUndefined();
+      expect(bcrypt.compareSync('NewPassword1', password)).toBe(true);
     });
   });
 });
